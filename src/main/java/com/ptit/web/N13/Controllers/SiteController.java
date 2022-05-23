24cd64da12d934309450a789eaab857b23f5cae4
Gn0hp <gn0hp289@gmail.com>
2022-05-23 15:30:13 +0700

    construct as spring starter project

---
+++ src/main/java/com/ptit/web/N13/Controllers/SiteController.java
@@ -0,0 +1,15 @@
+package com.ptit.web.N13.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class SiteController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+}
---
+++ src/main/java/com/ptit/web/N13/PtitWebBtlN13Application.java
@@ -0,0 +1,13 @@
+package com.ptit.web.N13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtitWebBtlN13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtitWebBtlN13Application.class, args);
+	}
+
+}
