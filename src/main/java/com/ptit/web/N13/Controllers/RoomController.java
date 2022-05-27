cef8bcbbeee999948bce721234544c8e067a361b
Gn0hp <gn0hp289@gmail.com>
2022-05-27 13:50:31 +0700

    feat(fe-router) set route from home to room-detail successfully, get data from database and pass to html by thymeleaf

--- src/main/java/com/ptit/web/N13/Controllers/RoomController.java
+++ src/main/java/com/ptit/web/N13/Controllers/RoomController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ptit.web.N13.Models.Room;
 import com.ptit.web.N13.Service.RoomService;
@@ -31,8 +33,13 @@ public class RoomController {
 	public String roomList() {
 		return "room-list";
 	}
-	@GetMapping("/room-details")
-	public String roomDetail() {
-		return "room-details";
+	@GetMapping("/room-detail/{id}")
+	public ModelAndView roomDetails(@PathVariable int id) {
+		ModelAndView mav = new ModelAndView("room-details");
+		Room room = roomService.findById(id);
+		mav.addObject("room", room);
+		System.out.print(room.getName());
+		return mav;
 	}
+	
 }
